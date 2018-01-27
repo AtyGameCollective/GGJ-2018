d7c9a7a2b33dd63333881db2daaf74f28d42086f
João Roberto - AcerRed <joaojrdev@gmail.com>
2018-01-27 19:01:50 -0200

    Level light controller

--- Game/Assets/Scripts/Player/PlayerLight.cs
+++ Game/Assets/Scripts/Player/PlayerLight.cs
@@ -7,72 +7,121 @@ namespace Aty
 {
     public class PlayerLight : MonoBehaviour
     {
+        [Header("Properties")]
+        [SerializeField]
+        private float LightTimeTotal = 60f;
+        [SerializeField]
+        private float lightTimeRemaining;
+
         private const float MaxPower = 300f;
 
-        [Header("Properties")]
+        [Header("Level Light")]
+        [SerializeField]
+        private Light _levelLight = null;
+        [SerializeField] private float _maxLevelLight = 300f;
+        [SerializeField] private float _minLevelLight = 0f;
 
-        [SerializeField] private Light      _lightObject   = null;
-        [SerializeField] private float      _power         = 300f;
-        [SerializeField] private float      _decreaseRate  = 10f;
+        [Header("Player Light")]
+        [SerializeField]
+        private Light _playerLight = null;
+        [SerializeField] private float _maxPlayerLight = 10f;
+        [SerializeField] private float _minPlayerLight = 0f;
+
+        [Header("Player Particles")]
+        [SerializeField]
+        private ParticleSystem _playerParticle;
+        private ParticleSystem.EmissionModule _playerParticleEmissor;
+        [SerializeField] private float _maxPlayerParticles = 30f;
+        [SerializeField] private float _minPlayerParticles = 0f;
+
+        [Header("Fog Color")]
+        [SerializeField] private Color _maxFogColor = Color.cyan;
+        [SerializeField] private Color _minFogColor = Color.black;
 
         [Header("Events")]
 
-        [SerializeField] private UnityEvent OnPowerDeplete = new UnityEvent();
+        [SerializeField]
+        private UnityEvent OnPowerDeplete = new UnityEvent();
 
-        public Light LightObject
+        public Light LevelLightObject
         {
             get
             {
-                return _lightObject;
+                return _levelLight;
             }
             set
             {
-                _lightObject = value;
+                _levelLight = value;
                 Refresh();
             }
         }
 
-        public float Power
+        public void LevelLightUpdate(float value)
+        {
+            _levelLight.range = Mathf.Lerp(_minLevelLight, _maxLevelLight, value);
+        }
+
+        public void PlayerLightUpdate(float value)
+        {
+            if(_playerLight)
+                _playerLight.range = Mathf.Lerp(_minLevelLight, _maxLevelLight, value);
+            if(_playerParticle)
+                _playerParticleEmissor.rateOverTime = Mathf.Lerp(_minPlayerParticles, _maxPlayerParticles, value );
+        }
+
+        public void FogUpdate(float value)
+        {
+            RenderSettings.fogColor = Color.Lerp(_minFogColor, _maxFogColor, value);
+        }
+
+        public float LightTimeRemaining
         {
             get
             {
-                return _power;
+                return lightTimeRemaining;
             }
+
             set
             {
-                _power = value.Clamp(0, MaxPower);
-                Refresh();
-                if (_power <= 0 && OnPowerDeplete != null) OnPowerDeplete.Invoke();
+                lightTimeRemaining = value;
             }
         }
 
         public void Refresh()
         {
-            if (_lightObject)
+            float lightValue = Mathf.Lerp(0, 1, LightTimeRemaining / LightTimeTotal);
+            
+            if (_levelLight)
             {
-                _lightObject.range = _power;
-                _lightObject.spotAngle = 180f * _power / MaxPower;
+                LevelLightUpdate(lightValue);
             }
-        }
+            if (_playerLight)
+            {
+                PlayerLightUpdate(lightValue);
+            }
+            FogUpdate(lightValue);
 
-        private void PowerDecrement()
-        {
-            if (Power > 0) Power -= _decreaseRate * Time.deltaTime;
+            if(lightValue <= 0 && OnPowerDeplete != null)
+            {
+                OnPowerDeplete.Invoke();
+            }
         }
 
         private void OnEnable()
         {
+            LightTimeRemaining = LightTimeTotal;
+            _playerParticleEmissor = _playerParticle.emission;
             Refresh();
         }
 
         private void Update()
         {
-            PowerDecrement();
-        }
+            if (LightTimeRemaining > 0)
+            {
+                LightTimeRemaining -= Time.deltaTime;
+                Refresh();
+            }
 
-        private void OnValidate()
-        {
-            Refresh();
         }
     }
 }
