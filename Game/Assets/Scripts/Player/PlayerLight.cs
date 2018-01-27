ff4e3c850a2aa10835390a02e6cd4f8240fddee1
Alexandre-PC\Alexandre <alexandrepetrassi@gmail.com>
2018-01-27 10:37:29 -0200

    Added PlayerLight Script

---
+++ Game/Assets/Scripts/Player/PlayerLight.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ATY
+{
+    public class PlayerLight : MonoBehaviour
+    {
+        [SerializeField] private Light _lightObject          = null;
+        [SerializeField] private float _power                = 300f;
+
+        public void Refresh()
+        {
+            if (_lightObject) _lightObject.range = _power;
+        }
+
+        public Light LightObject
+        {
+            get
+            {
+                return _lightObject;
+            }
+            set
+            {
+                _lightObject = value;
+                Refresh();
+            }
+        }
+
+        public float Power
+        {
+            get
+            {
+                return _power;
+            }
+            set
+            {
+                _power = value;
+                Refresh();
+            }
+        }
+
+        private void OnEnable()
+        {
+            Refresh();
+        }
+
+        private void OnValidate()
+        {
+            Refresh();
+        }
+    }
+}
