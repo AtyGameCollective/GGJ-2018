23b1707c88769581a89af57c14454bda82dbf28a
Alexandre-PC\Alexandre <alexandrepetrassi@gmail.com>
2018-01-27 11:52:28 -0200

    Added LightBeam Script

---
+++ Game/Assets/Scripts/NPCs/LightBeam.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ATY
+{
+    public class LightBeam : MonoBehaviour
+    {
+        [SerializeField] private float    _power = 50;
+        [SerializeField] private Animator beamAnimator;
+
+        private Collider _myCollider = null;
+        private Collider myCollider
+        {
+            get
+            {
+                if(!_myCollider) _myCollider = GetComponent<Collider>();
+                return _myCollider;
+            }
+        }
+
+        public float Power
+        {
+            get
+            {
+                return _power;
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.collider.CompareTag("player")) collideWith(collision.collider.GetComponent<PlayerLight>());
+            
+        }
+
+        private void collideWith(PlayerLight player)
+        {
+            if (!player) return;
+            player.Power += Power;
+            myCollider.enabled = false;
+        }
+
+        private void triggerFadeAnimation()
+        {
+            if (beamAnimator) beamAnimator.SetBool("fade",true);
+        }
+
+        private void resetFadeAnimation()
+        {
+            if (beamAnimator) beamAnimator.SetBool("fade", false);
+        }
+
+        private void OnEnable()
+        {
+            resetFadeAnimation();
+            myCollider.enabled = true;
+        }
+    }
+}
