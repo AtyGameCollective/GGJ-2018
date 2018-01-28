006a87eebb0877f51a5fd51fa13d63a700c97e2c
Alexandre-PC\Alexandre <alexandrepetrassi@gmail.com>
2018-01-28 05:40:10 -0200

    Added automated script generator to automatically create Tags and Layers enums

---
+++ Game/Assets/Scripts/Editor/CodeGenerator.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.IO;
+using CodeGeneration;
+using UnityEditor;
+using System;
+using System.Linq;
+
+namespace AssemblyCSharpEditor
+{
+    /// <summary>
+    /// A helper class for code generation in the editor.
+    /// </summary>
+    public class CodeGenerator
+    {
+        [MenuItem("Code Generation/Generate Tags")]
+        public static void GenerateTags()
+        {
+            Generate("Tags", UnityEditorInternal.InternalEditorUtility.tags);
+        }
+
+        [MenuItem("Code Generation/Generate Layers")]
+        public static void GenerateLayers()
+        {
+            Generate("Layers", UnityEditorInternal.InternalEditorUtility.layers);
+        }
+
+        public static void Generate(string name, string[] data)
+        {
+            // Build the generator with the class name and data source.
+            StringItemsGenerator generator = new StringItemsGenerator(name, data);
+
+            // Generate output (class definition).
+            var classDefintion = generator.TransformText();
+
+            var outputPath = Path.Combine(Application.dataPath + "/Scripts/AUTO_GEN/", name + ".cs");
+
+            try
+            {
+                // Save new class to assets folder.
+                File.WriteAllText(outputPath, classDefintion);
+
+                // Refresh assets.
+                AssetDatabase.Refresh();
+
+                Debug.Log("File Generated: " + name + ".cs");
+            }
+            catch (Exception e)
+            {
+                Debug.Log("An error occurred while saving file: " + e);
+            }
+        }
+    }
+}
