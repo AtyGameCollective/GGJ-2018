062c40b5c1cade2d7d9f5ce757719b16d983570e
Alexandre-PC\Alexandre <alexandrepetrassi@gmail.com>
2018-01-27 23:15:10 -0200

    Added HeatDrain script

---
+++ Game/Assets/Scripts/NPCs/HeatDrain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Aty
+{
+    [RequireComponent(typeof(Heat))]
+    public class HeatDrain : MonoBehaviour
+    {
+
+        private Heat _heat = null;
+        private Heat heat
+        {
+            get
+            {
+                if (!_heat) _heat = GetComponent<Heat>();
+                return _heat;
+            }
+        }
+
+        private void Update()
+        {
+            if (heat.CurrentHeat > 0) heat.CurrentHeat -= Time.deltaTime;
+        }
+    }
+}
