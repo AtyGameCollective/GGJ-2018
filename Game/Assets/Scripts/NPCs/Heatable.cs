888c070891407d115cccfaacdef5a78c74845404
Alexandre-PC\Alexandre <alexandrepetrassi@gmail.com>
2018-01-27 23:13:23 -0200

    Added Heatable abstract class

---
+++ Game/Assets/Scripts/NPCs/Heatable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Aty
+{
+    [System.Serializable]
+    public abstract class Heatable : MonoBehaviour
+    {
+        public abstract void OnHeatChange(float percent);
+    }
+}
